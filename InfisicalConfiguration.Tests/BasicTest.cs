d7eda65062a6e58b7f40c9bb8fc98d4769eb1e67
Daniel Hougaard <daniel@infisical.com>
2025-01-21 04:17:41 +0100

    Update BasicTest.cs

--- InfisicalConfiguration.Tests/BasicTest.cs
+++ InfisicalConfiguration.Tests/BasicTest.cs
@@ -18,15 +18,15 @@ public class SystemTests
     var configuration = new ConfigurationBuilder()
     .AddInfisical(
         new InfisicalConfigBuilder()
-            .SetProjectId("d3a577f2-efd7-49b4-a345-5e5aed564fc7")
+            .SetProjectId("<project-id>")
             .SetEnvironment("dev")
             .SetSecretPath("/")
             .SetInfisicalUrl("http://localhost:8080")
             .SetAuth(
                 new InfisicalAuthBuilder()
                     .SetUniversalAuth(
-                        "fe5ed60a-76ca-4148-814e-05c0a84254c3",
-                        "92d7de0e11e5cf7790de93780e4c1e594a639e9a0a45c1669191fc24a132ae08"
+                        "<machine-identity-client-id>",
+                        "<machine-identity-client-secret>"
                     )
                     .Build()
             )
